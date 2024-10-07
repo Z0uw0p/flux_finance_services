000e620b9c633adef3599f2683c6f996debeb631
Z0uw0p <mathis.tchepannou@gmail.com>
2024-10-07 13:18:06 -0400

    added user and password

--- database_creation/market_data.py
+++ database_creation/market_data.py
@@ -5,7 +5,7 @@ sec_ticker_list = pd.read_csv(r"ticker_lists\sec_tickers.csv")['Ticker'].to_list
 
 ticker_list = tse_ticker_list + sec_ticker_list
 
-user = ""
-password = ""
+user = "flux-finance-market-database"
+password = 'f8v7#P6nbe"x9.1T'
 host = ""
 database = ""
